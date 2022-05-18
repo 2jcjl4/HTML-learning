6317ed8eabb4ffd531f469e2da096e16341ddaaf
James <2jcjl4@gmail.com>
2022-05-18 10:25:35 +0100

    Starting JS and uploading the exercises 1-3

---
+++ JS/jsintro.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+// semicolons are not needed at the end to close them off in JS it works either way
+console.log("im using this console to learn");
+console.info("this is a info console");
+console.warn("this is a warn console");
+console.error("this is a error console");
